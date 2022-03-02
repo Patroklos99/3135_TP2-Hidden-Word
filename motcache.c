8b64c8242d4debd943fd14bbf2c0bde6d2529226
Salcedo Salcedo, Renzo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-03-02 11:01:18 -0500

    Ajout fichier motcache.c

---
+++ motcache.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int c;
+    FILE *file;
+    file = fopen(argv[1], "r");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return 0;
+}
