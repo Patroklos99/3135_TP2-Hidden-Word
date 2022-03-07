7b2dcfd4339f10eb7d8c84b5da1a08fa8c87d3e0
Salcedo Salcedo, Renzo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-03-06 21:05:04 -0500

    ft ()modifier et modif trouverMot

--- motcache.c
+++ motcache.c
@@ -3,12 +3,11 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <ctype.h>
 
 typedef struct {
-    int debutX;
-    int debutY;
-    int finX;
-    int finY;
+    char *key;
+    int valeur;
 } positions;
 
 void creerTabMots(char tab[13][13], FILE *file, int *compt) {
@@ -35,51 +34,60 @@ void obtenirMots(char tab[150][20], FILE *file, const int *compt) {
     (fgets(tab[i], 13, file));
 }
 
-/*bool horizontal(char tab[150][20], char bankMots[13][13]) {
-    bool reponse = true;
-    int w = 0;
-    int b = 0;
-    int a;
-    for (a = 0; a <= strlen(tab[w]) - 1; a++) {
-        for (w = 0; w <= strlen(tab[w]) - 1; ++w) {
-            if (tab[0][w] != bankMots[a][b]) {
-                reponse = false;
-                break;
-            }
-            a++;
-        }
-    }
-    return reponse;
-}*/
+void archiverPositions(int x, int y, positions positions1) {
+    const char key[] = "a";
+    positions1.key = malloc(sizeof(key));
+    memcpy(positions1.key, key, sizeof(key));
 
-void archiverPositions(int x, int y) {
-
+    positions1.valeur = x;
+    printf("%s%d ", positions1.key, positions1.valeur);
 }
 
 void identifierPositions() {
 
 }
 
-bool trouverMot(char tab[150][20], char bankMots[13][13], int x, int y, int longueurMatch, int motNumero, positions positions1) {
+void modifier(char bank[13][13], int x, int y) {
+    /*printf*/("Positon trouve a (%d:%d)\n", x, y);
+    bank[y][x] = (char) tolower(bank[y][x]);
+    /*for (int b = 0; b <= 11; b++) {
+        for (int a = 0; a <= 12; a++)
+            printf("%c", bank[a][b]);
+    }*/
+}
+
+trouverMot(char tab[150][20], char bankMots[13][13], int x, int y, int longueurMatch, int motNumero, positions
+positions1) {
+
+    /*printf("recherche du mot: %s\n", tab[motNumero]);
+    for (int b = 0; b <= 11; b++) {
+        for (int a = 0; a <= 12; a++)
+            printf("%c", bankMots[a][b]);
+    }*/
     int longueurMot = (int) strlen(tab[motNumero]) - 1;
-    if (longueurMot == longueurMatch){
+    if (longueurMot == longueurMatch) {
         identifierPositions();
+        /*for (int b = 0; b <= 11; b++) {
+            for (int a = 0; a <= 12; a++)
+                printf("%c", bankMots[a][b]);
+        }*/
         return true;
     }
-    if (x < 0 || y < 0 || x >= 12 || y >= 12)
+    if (x < 0 || y < 0 || x >= 13 || y >= 13)
         return false;
 
-    if (bankMots[y][x] == tab[motNumero][longueurMatch]) {
-        archiverPositions(x, y);
-                // marké comme visité
-        char temp = tab[x][y];
+    if ((bankMots[y][x] | 0x20) == (tab[motNumero][longueurMatch] | 0x20)) {
+        //archiverPositions(x, y, positions1);
+        printf("recherche de lettre %c\n", bankMots[y][x]);
+        modifier(bankMots, x, y);
+        // marké comme visité
+        //char temp = tab[x][y];
         //tab[x][y] = '#';
         bool answer = trouverMot(tab, bankMots, x - 1, y, (longueurMatch + 1), motNumero, positions1) |
-                      trouverMot(tab, bankMots, x + 1, y, (longueurMatch + 1), motNumero, positions1)|
+                      trouverMot(tab, bankMots, x + 1, y, (longueurMatch + 1), motNumero, positions1) |
                       trouverMot(tab, bankMots, x, y - 1, (longueurMatch + 1), motNumero, positions1) |
                       trouverMot(tab, bankMots, x, y + 1, (longueurMatch + 1), motNumero, positions1);
-        // marké comme non visité encore
-        tab[x][y] = temp;
+        //tab[x][y] = temp;
         return answer;
     } else
         return false;
@@ -87,19 +95,19 @@ bool trouverMot(char tab[150][20], char bankMots[13][13], int x, int y, int long
 
 void trouverPremierLettre(char tab[150][20], char bankMots[13][13], positions positions1) {
     int w = 0;
-        int longueurMatch = 0;
-        for (int nligne = 0; nligne <= 11; nligne++) {
-            for (int ncol = 0; ncol <= 12; ncol++) {
-                if (tab[w][0] == bankMots[ncol][nligne]) {
-                    if (trouverMot(tab, bankMots, nligne, ncol, longueurMatch, w, positions1)) {
-                        printf("mot trouve -> %s", tab[w]);
-                        w++;
-                        ncol = -1;
-                        nligne = 0;
-                    }
+    int longueurMatch = 0;
+    for (int nligne = 0; nligne <= 11; nligne++) {
+        for (int ncol = 0; ncol <= 12; ncol++) {
+            if ((tab[w][0] | 0x20) == (bankMots[ncol][nligne] | 0x20)) {
+                if (trouverMot(tab, bankMots, nligne, ncol, longueurMatch, w, positions1)) {
+                    printf("mot trouve -> %s", tab[w]);
+                    w++;
+                    ncol = -1;
+                    nligne = 0;
                 }
             }
         }
+    }
 }
 
 void rechercheMots(char tab[150][20], char bankMots[13][13], positions positions1) {
@@ -111,25 +119,32 @@ void lireFichier(char **argv, char tab[150][20], positions positions1) {
     int compteur = 0;
     char bankMots[13][13];
 
+
     while (file && compteur <= 33) {
         //line[i][strlen(line[i]) - 1] = '\0';
         compteur <= 12 ? creerTabMots(bankMots, file, (int *) &compteur) : obtenirMots(tab, file, (int *) &compteur);
         compteur++;
     }
+    fclose(file);
+    rechercheMots(tab, bankMots, positions1);
     for (int b = 0; b <= 11; b++) {
         for (int a = 0; a <= 12; a++)
             printf("%c", bankMots[a][b]);
     }
-    fclose(file);
-    rechercheMots(tab, bankMots, positions1);
+    for (int b = 0; b <= 11; b++) {
+       for (int a = 0; a <= 12; a++)
+            if (isupper(bankMots[a][b])) {
+                printf("%c", bankMots[a][b]);
+            }
+    }
 }
 
 int main(int argc, char *argv[]) {
     char tab[150][20];
-    positions pos1= {0, 0, 0, 0};
+    positions pos1 = {0};
     lireFichier(argv, tab, pos1);
     /*for (int w = 0; w <= 20; ++w) {
         printf("%s", tab[w]);
     }*/
     return 0;
-}
+} 
