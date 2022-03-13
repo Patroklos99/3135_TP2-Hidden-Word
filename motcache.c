7200bb7211492e2a12f1a70ca5bdd196a9e830aa
Salcedo Salcedo, Renzo <salcedo_salcedo.renzo@courrier.uqam.ca>
2022-03-13 00:58:21 -0500

    modif snakecases motcache.c

--- motcache.c
+++ motcache.c
@@ -32,58 +32,65 @@ void obtenir_mots(char tab[150][45], FILE *file, const int *compt) {
    (fgets(tab[i], 14, file));
 }
 
-void modifier(char bank[13][13], int x, int y) {
+void modifier_minuscule(char bank[13][13], int x, int y) {
    bank[y][x] = (char) tolower(bank[y][x]);
 }
 
-bool trouver_mot(char tab[150][45], char bankMots[13][13], int x, int y, int longueurMatch, int motNumero, enum dir 
+bool trouver_mot(char tab[150][45], char bank_mots[13][13], int x, int y, int longueur_match, int mot_numero, enum dir
         dir) {
-   int longueurMot = (int) strlen(tab[motNumero]) - 1;
-   if (longueurMot == longueurMatch)
+   int longueur_mot = (int) strlen(tab[mot_numero]) - 1;
+   if (longueur_mot == longueur_match)
       return true;
    if (x < 0 || y < 0 || x >= 13 || y >= 13)
       return false;
-   if ((bankMots[y][x] | 0x20) == (tab[motNumero][longueurMatch] | 0x20)) {
+   if ((bank_mots[y][x] | 0x20) == (tab[mot_numero][longueur_match] | 0x20)) {
       bool answer;
       switch (dir) {
          case N:
-            answer = trouver_mot(tab, bankMots, x + 1, y,
-                                 (longueurMatch + 1), motNumero, N);
+            answer = trouver_mot(tab, bank_mots, x + 1, y,
+                                 (longueur_match + 1), mot_numero, N);
             break;
          case S:
-            answer = trouver_mot(tab, bankMots, x - 1, y,
-                                 (longueurMatch + 1), motNumero, S);
+            answer = trouver_mot(tab, bank_mots, x - 1, y,
+                                 (longueur_match + 1), mot_numero, S);
             break;
          case O:
-            answer = trouver_mot(tab, bankMots, x, y - 1,
-                                 (longueurMatch + 1), motNumero, O);
+            answer = trouver_mot(tab, bank_mots, x, y - 1,
+                                 (longueur_match + 1), mot_numero, O);
             break;
          case E:
-            answer = trouver_mot(tab, bankMots, x, y + 1,
-                                 (longueurMatch + 1), motNumero, E);
+            answer = trouver_mot(tab, bank_mots, x, y + 1,
+                                 (longueur_match + 1), mot_numero, E);
             break;
       }
       if (answer)
-         modifier(bankMots, x, y);
+         modifier_minuscule(bank_mots, x, y);
       return answer;
    } else
       return false;
 }
 
-void trouver_premier_lettre(char tab[150][45], char bankMots[13][13]) {
-   int motNumero = 0;
-   int longueurMatch = 0;
+void rechercher_4sens(char tab_mots[150][45], char bank_mots[13][13], int *nligne, int *ncol, int *mot_numero) {
+   int longueur_match = 0;
+   int nligne1 = *nligne;
+   int ncol1 = *ncol;
+   int mot_numero1 = *mot_numero;
+   if (trouver_mot(tab_mots, bank_mots, nligne1, ncol1, longueur_match, mot_numero1, E)
+       || (trouver_mot(tab_mots, bank_mots, nligne1, ncol1, longueur_match, mot_numero1, O))
+       || trouver_mot(tab_mots, bank_mots, nligne1, ncol1, longueur_match, mot_numero1, N)
+       || trouver_mot(tab_mots, bank_mots, nligne1, ncol1, longueur_match, mot_numero1, S)) {
+      ++*mot_numero;
+      *ncol = 0;
+      *nligne = -1;
+   }
+}
+
+void trouver_premier_lettre(char tab_mots[150][45], char bank_mots[13][13]) {
+   int mot_numero = 0;
    for (int nligne = 0; nligne <= 11; nligne++) {
       for (int ncol = 0; ncol <= 12; ncol++) {
-         if ((tab[motNumero][0] | 0x20) == (bankMots[ncol][nligne] | 0x20)) {
-            if (trouver_mot(tab, bankMots, nligne, ncol, longueurMatch, motNumero, E)
-                || (trouver_mot(tab, bankMots, nligne, ncol, longueurMatch, motNumero, O))
-                || trouver_mot(tab, bankMots, nligne, ncol, longueurMatch, motNumero, N)
-                || trouver_mot(tab, bankMots, nligne, ncol, longueurMatch, motNumero, S)) {
-               motNumero++;
-               ncol = 0;
-               nligne = -1;
-            }
+         if ((tab_mots[mot_numero][0] | 0x20) == (bank_mots[ncol][nligne] | 0x20)) {
+            rechercher_4sens(tab_mots, bank_mots, &nligne, &ncol, &mot_numero);
          }
       }
    }
@@ -130,12 +137,12 @@ void valider_fichier(int argc) {
 }
 
 void lire_fichier(char **argv) {
-   char tab[150][45];
-   char bankMots[13][13];
+   char tab_mots[150][45];
+   char bank_mots[13][13];
    FILE *file = fopen(argv[1], "r");
-   creer_tableaux(file, tab, bankMots);
-   recherche_mots(tab, bankMots);
-   afficher_mot(bankMots);
+   creer_tableaux(file, tab_mots, bank_mots);
+   recherche_mots(tab_mots, bank_mots);
+   afficher_mot(bank_mots);
 }
 
 int main(int argc, char *argv[]) {
@@ -143,4 +150,3 @@ int main(int argc, char *argv[]) {
    lire_fichier(argv);
    return 0;
 }
-
